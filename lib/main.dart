a361e678cdea5c23443e379d5a01d58aec9e52f4
Andrei Popovici <me@andreipopovici.co.uk>
2020-01-15 08:00:00 +0000

    Basic Magic8 skeleton created

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,25 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+            title: Text('8 Magic ball'),
+            backgroundColor: Colors.teal,
+          ),
+          body: Magic8Ball(),
+        ),
       ),
     );
+
+class Magic8Ball extends StatefulWidget {
+  @override
+  _Magic8BallState createState() => _Magic8BallState();
+}
+
+class _Magic8BallState extends State<Magic8Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
